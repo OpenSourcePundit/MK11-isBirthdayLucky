70807199b47932eb594a62b1e024bb32b0aa87c3
OpenSourcePundit <shashank00098@gmail.com>
2022-12-05 11:17:24 +0530

    JS file added and updated.
    script line position changed. also JS file is added . site working perfect now

---
+++ birthdayLucky.js
@@ -0,0 +1,23 @@
+const input = document.querySelectorAll('.input') ;
+const checkNumberBtn = document.querySelector('#checkNumber');
+const output = document.querySelector('#output');
+
+function dobSum(dob){
+    dob = dob.replaceAll("-","");
+    var sum=0;
+    for(index=0;index<dob.length;index++){
+        sum= sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+function checkBirthday(){
+    var dob = dobSum(input[0].value);
+    var luckyNumber = Number(input[1].value);
+    if(dob===luckyNumber){
+        output.innerText="YESS..!! Your birthday is LUCKY..😎🤩!!"
+    } else{
+        output.innerText="NOOOO..!! Its not Lucky..!! Do try it in next Life.!! 😋😛 "
+    }
+}
+checkNumberBtn.addEventListener('click',checkBirthday)
